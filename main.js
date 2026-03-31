cbaae8adb339742430ef5d68ac3f0e7ae080d094
Pradeep Singh <pradeepsingh1@gmail.com>
2026-04-01 07:35:27 +1100

    6: add legend and improve map rendering

--- main.js
+++ main.js
@@ -22,6 +22,8 @@ function render(metric) {
     const color = d3.scaleSequential(d3.interpolateReds)
         .domain([0,1]);
 
+    svg.selectAll("*").remove();
+
     svg.selectAll("path")
         .data(geoData.features)
         .join("path")
@@ -46,4 +48,42 @@ function render(metric) {
         .on("mouseout", function() {
             d3.selectAll(".tooltip").remove();
         });
+
+    // Legend
+    const legendWidth = 200;
+    const legendHeight = 10;
+
+    const legendScale = d3.scaleLinear()
+        .domain([0,1])
+        .range([0, legendWidth]);
+
+    const legend = svg.append("g")
+        .attr("transform", "translate(20,20)");
+
+    const gradient = svg.append("defs")
+        .append("linearGradient")
+        .attr("id", "gradient");
+
+    gradient.selectAll("stop")
+        .data(d3.range(0, 1.01, 0.1))
+        .enter()
+        .append("stop")
+        .attr("offset", d => d * 100 + "%")
+        .attr("stop-color", d => color(d));
+
+    legend.append("rect")
+        .attr("width", legendWidth)
+        .attr("height", legendHeight)
+        .style("fill", "url(#gradient)");
+
+    const axis = d3.axisBottom(legendScale)
+        .ticks(5);
+
+    legend.append("g")
+        .attr("transform", `translate(0,${legendHeight})`)
+        .call(axis);
+
+    legend.append("text")
+        .attr("y", -5)
+        .text(metric.toUpperCase());
 }
